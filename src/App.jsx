d6936b5e82f469f33651a8aafdab4920c8e3eeb6
Xiu8insky <106526261+8insky@users.noreply.github.com>
2024-12-30 15:34:01 +0100

    Creating About section and Navbar section
    Creating About section and Navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Hero from "./components/Hero"
+import About from "./components/About"
+import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
-    <main>
+    <main className="bg-zinc-400">
 
+      <Navbar/>
       <Hero className ='relative min-h-screen w-screen overflow-x-hidden '/>
+      <About/>
     </main>
   )
 }
